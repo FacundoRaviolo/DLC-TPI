0831d19a65e9ed744de3a2b72f3a70ad5a4528b8
FacundoRaviolo <raviolofacundo@gmail.com>
2019-05-31 02:18:42 -0300

    Añadidas las primeras funciones del buscador y las primeras consultas. Comentarios sobre algunas cosas que deberíamos modificar.

---
+++ src/main/java/Buscador.java
@@ -0,0 +1,44 @@
+import entidades.VocabularioEntity;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import java.util.*;
+
+    // ESTO ESTÁ MAL!!!! SI YO ARMO UNA HashMap (o HashTable) Y DOS PALABRAS APARECEN EN LA MISMA CANTIDAD DE DOCUMENTOS,
+    // AL SER cantDoc LA CLAVE, NO SE PODRÁN GUARDAR AMBOS!!! CORREGIR. BUSCAR OTRA MANERA
+
+public class Buscador {
+    public void busqueda(String consulta, Hashtable<String, VocabularioEntity> tablaHash)
+    {
+        HashMap<Integer,VocabularioEntity> hashMap = new HashMap<>();
+        for (String word : consulta.split(" "))
+        {
+            VocabularioEntity termino = tablaHash.get(word);
+            int cantDoc = termino.getCantDoc();
+            hashMap.put(cantDoc,termino);
+        }
+
+        obtencionCandidatos(hashMap);
+    }
+
+    public void obtencionCandidatos(HashMap<Integer,VocabularioEntity> hashMap)
+    {
+        Iterator iterator = hashMap.entrySet().iterator();
+        Persistencia persistencia = new Persistencia();
+        EntityManager em = persistencia.crearPersistencia();
+        while (iterator.hasNext())
+        {
+            Map.Entry termino = (Map.Entry) iterator.next();
+            VocabularioEntity voc = (VocabularioEntity)termino.getValue();
+            String palabra = voc.getPalabra();
+            int cantDoc = (int) termino.getKey();
+            Query query = em.createQuery("SELECT (posteo.vecesEnDoc) FROM PosteoEntity posteo WHERE posteo.palabra = '" + palabra + "' ORDER BY posteo.vecesEnDoc DESC");
+            List lista = query.setMaxResults(5).getResultList();
+            System.out.println("Lista de " + palabra + ": " + lista.toString());
+        }
+        persistencia.cerrarPersistencia(em);
+        System.out.println(hashMap.toString());
+    }
+
+
+}
--- src/main/java/PruebasBD.java
+++ src/main/java/PruebasBD.java
@@ -7,6 +7,7 @@ import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Hashtable;
+import java.util.List;
 
 public class PruebasBD {
 
@@ -14,30 +15,29 @@ public class PruebasBD {
     public static void main(String[] args) throws Exception {
 
         // Las siguientes instrucciones realizan la creación de la HashTable con sus datos, y almacenan en la BD los Documentos, el Vocabulario y los Posteos.
-        //long startTime = System.nanoTime();
+        /*
+        long startTime = System.nanoTime();
         Persistencia persistencia = new Persistencia();
-      //  EntityManager em = persistencia.crearPersistencia();
-        //Hashtable<String,VocabularioEntity> tablaHash = new Hashtable<>();
-       // Parseo parseo = new Parseo();
-       // File carpeta = new File("DocumentosTP");
-      //  parseo.ObtenerDatosDocumento(em,carpeta,tablaHash,false);
-       // persistencia.persistirVocabulario(em,tablaHash);
-       // persistencia.persistirPosteo(em,carpeta);
-      //  persistencia.cerrarPersistencia(em);
-      //  long endTime = System.nanoTime() - startTime;
-       // System.out.println("Tiempo final: " + endTime);
-       // persistencia.serializarTabla(tablaHash);
-/*      HashMap<String,Integer> listaPosteo = new HashMap<String,Integer>();
+        EntityManager em = persistencia.crearPersistencia();
+        Hashtable<String,VocabularioEntity> tablaHash = new Hashtable<>();
+        Parseo parseo = new Parseo();
+        File carpeta = new File("DocumentosTP");
+        parseo.ObtenerDatosDocumento(em,carpeta,tablaHash,true);
+        persistencia.persistirVocabulario(em,tablaHash);
+        persistencia.persistirPosteo(em,carpeta);
+        persistencia.cerrarPersistencia(em);
+        long endTime = System.nanoTime() - startTime;
+        System.out.println("Tiempo final: " + endTime);
 
-        //File file1 = new File("DocumentosTP/0ddc809a.txt");
-        //parseo.parseador(file1,tablaHash);
-
-        //System.out.println(tablaHash.get("and"));
-        // System.out.println(tablaHash.size());
-
+        persistencia.serializarTabla(tablaHash);
         */
+        Persistencia persistencia = new Persistencia();
         Hashtable<String,VocabularioEntity> hT1 = persistencia.leerTabla("tabla.dat");
-        System.out.println(hT1.get("aber"));
+        System.out.println("Hashtable armada");
 
+        Buscador buscador = new Buscador();
+        // HABRÍA QUE CHEQUEAR QUE LA CONSULTA SIEMPRE SE TRANSFORME A MINÚSCULA!!!!!!!!!!!!!!!!!
+        String consulta = "while a and e sanctae";
+        buscador.busqueda(consulta, hT1);
     }
 }
